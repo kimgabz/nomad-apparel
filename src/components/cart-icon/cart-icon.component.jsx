01ddc885183e75905878b66fd7349fb0856e6928
Kim Harold Gabiana <KIMG@MacBooks-MacBook-Pro.local>
2020-03-13 16:29:19 +0800

    selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
@@ -15,5 +17,13 @@ const CartIcon = ({ toggleCartHidden }) => (
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
-})
-export default connect(null, mapDispatchToProps)(CartIcon);
+});
+
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CartIcon);
