33b455ae823a46f5bbeb41bd9e6725e15f85b4c3
Kim Harold Gabiana <KIMG@MacBooks-MacBook-Pro.local>
2020-03-24 13:47:40 +0800

    styled components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,16 @@
 import  React from 'react';
 
 import Directory from '../../components/directory/directory.component'
-import './homepage.styles.scss';
+// import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles'
 
 const HomePage = () => (  
-  <div className='homepage'>
-    {/* <h1>Welcome to my Homepage</h1> */}
+  // <div className='homepage'>
+  //   <Directory />
+  // </div>
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
